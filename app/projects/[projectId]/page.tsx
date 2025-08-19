257dde4cfdccc7308e4c2d70093db13f894396ed
ghostpete <ghostcoderme@gmail.com>
2025-08-19 09:59:57 +0100

    added revealonscroll and project details and loader

--- app/projects/[projectId]/page.tsx
+++ app/projects/[projectId]/page.tsx
@@ -1,14 +1,14 @@
 import { Project } from "@/lib/types";
 import { notFound } from "next/navigation";
-
 import { getProjectByID } from "@/actions/getProjectByID";
-import { ProjectParams } from "@/lib/types";
 import ProjectClient from "../_components/ProjectClient";
 
 export default async function ProjectDetails({
-  params: { projectId },
-}: ProjectParams) {
-  const projectArray = await getProjectByID(projectId);
+  params,
+}: {
+  params: { projectId: string };
+}) {
+  const projectArray = await getProjectByID(params.projectId);
   const project: Project | undefined = projectArray[0];
 
   if (!project) return notFound();
